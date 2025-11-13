038820758d7ab30f2b9b57da94c1ddbc17ee895f
Cesar Augusto <157239086+c3s4rAugust0@users.noreply.github.com>
2025-11-12 23:43:51 -0300

    Add files via upload

---
+++ Projeto/script.ts
@@ -0,0 +1,10 @@
+// Rolagem suave ao clicar em links do menu (se quiser adicionar futuramente)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
